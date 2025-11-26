58dc7a34fe4da04245d064f0d436d2f08efecc2b
unknown <max.jauhiainen@tuni.fi>
2025-11-26 12:15:49 +0200

    More tests

--- test/unitTests.js
+++ test/unitTests.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import add from "../src/add.js";
 import divide from "../src/divide.js";
+import toString from "../src/toString.js"
 
 describe('Pre planned tests', function () {
 
@@ -31,6 +32,32 @@ describe('Pre planned tests', function () {
     });
     it("Dividing zero with itself", function () {
       expect(divide(0,0)).to.be.NaN;
+    });
+  });
+});
+
+describe("Tests planned in phase 2", function () {
+  describe("Tests for toString()", function () {
+    it("Basic functionality", function () {
+      expect(toString(6)).to.equal("6");
+    });
+    it("Appending strings", function () {
+      expect("Earth-" + toString(616)).to.equal("Earth-616")
+    });
+    it("Stringifying lists", function () {
+      expect(toString([1,2,3])).to.equal("1,2,3")
+    });
+    it("Stringifying a string", function () {
+      expect(toString("world")).to.equal("world");
+    });
+    it("Stringifying null values", function () {
+      expect(toString(null)).to.equal("");
+    });
+    it("Stringifying undefined values", function () {
+      expect(toString(undefined)).to.equal("");
+    });
+    it("Stringifying a list with null value included", function () {
+      expect(toString([null,1,2])).to.equal(",1,2");
     });
   });
 });
